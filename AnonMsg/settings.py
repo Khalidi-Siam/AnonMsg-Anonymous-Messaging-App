601b2503a2114ad15b84a6e8500d9f028cc366b9
Khalidi-Siam <siam074@yahoo.com>
2025-09-29 03:23:35 +0600

    allow all host for test

--- AnonMsg/settings.py
+++ AnonMsg/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['anonmsg.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'user_app.User'
 
